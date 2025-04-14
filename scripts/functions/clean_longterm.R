f32ca5dd477466a05545c20871fffffa1fe8864e
matteo-torres <matteotorres27@gmail.com>
2025-04-14 22:36:56 +0000

    fix function

--- scripts/functions/clean_longterm.R
+++ scripts/functions/clean_longterm.R
@@ -12,25 +12,19 @@
 #' @note Define input and output file paths
 #' 
 #' @examples
-#' clean_longterm()
+#' clean_longterm(raw_data)
 #' 
- clean_longterm <- function() {
-   
-   # Define input and output file paths
-   input_path <- "data/raw/MARINe_data/longterm/phototranraw_download.csv"
-   output_path <- "data/processed/MARINe_data/longterm/longterm_processed.csv"
-   
-   # Read raw long-term survey data
-   raw_data <- read_csv(input_path)
+ clean_longterm <- function(raw_data) {
    
    # Clean raw survey data
    longterm_processed <- raw_data %>%
      filter(state_province == "California" & island == "Mainland") %>%
-     filter(!lumping_code %in% c("ROCK", "SAND", "TAR", "NONCRU")) %>%
+     filter(!lumping_code %in% c("ROCK", "SAND", "TAR", "NONCRU", "OTHALG", "OTHBAR", 
+                                 "OTHBRO", "OTHGRE", "OTHINV", "OTHRED", "OTHSUB")) %>%
      mutate(year = year(survey_date))
    
    # Save cleaned long-term survey data to "processed" data folder
-   write_csv(longterm_processed, output_path)
+   write_csv(longterm_processed, here("data", "processed", "MARINe_data", "longterm", "longterm_processed.csv"))
    
    return(longterm_processed)
    
