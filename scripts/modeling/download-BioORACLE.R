4130c4cfe8df81ef6d8bee153b320bc36f2db6ae
jcsibley <jordan.c.sibley@gmail.comm>
2025-04-23 18:34:53 +0000

    Initial commit

---
+++ scripts/modeling/download-BioORACLE.R
@@ -0,0 +1,15 @@
+# Downloading Bio-ORACLE environmental rasters
+# Author: Jordan Sibley 
+# File created: April 23, 2025 
+
+# -----------------------------------------------------------------------------
+
+# This document go through the process of downloading the environmental variables
+# from Bio-ORACLE 
+
+# Historical data time period: 
+# Future projected data: 
+
+
+# -----------------------------------------------------------------------------
+
