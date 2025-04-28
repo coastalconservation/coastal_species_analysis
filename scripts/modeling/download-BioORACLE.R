f8ec492cb2849bb87a803fb3add23be135ce8005
jcsibley <jordan.c.sibley@gmail.comm>
2025-04-28 05:09:45 +0000

    Spatial preprocessing and download rasters to cyberduck

--- scripts/modeling/download-BioORACLE.R
+++ scripts/modeling/download-BioORACLE.R
@@ -5,10 +5,10 @@
 # -----------------------------------------------------------------------------
 
 # This document go through the process of downloading the environmental variables
-# from Bio-ORACLE 
+# from Bio-ORACLE, and to do some spatial pre processing to prepare them for SDMs
 
 # Historical data time period: 2000-2010 (only goes in 10 year increments)
-# Future projected data: 
+# Future projected data: 2050 - 2060 under SSP 460
 
 
 # Environmental rasters 
@@ -22,19 +22,19 @@
 
 # -----------------------------------------------------------------------------
 
-# load libraries 
+# Load libraries 
 library(biooracler)
 library(here)
 library(purrr)
 library(terra)
+library(sf)
 library(testthat)
 
 
+# Read in coastal buffer shapefile from CyberDuck
+ca_coast <- read_sf('/capstone/coastalconservation/data/raw/spatial_data/CA_coastal_buffer_modeling/coast.buff.shp')
 
 # Historical data -------------------------------------------------------------
-
-### NOTES: looked online and it seems that the range of these files are only from 2000 to 2010
-
 
 # Define layers of interest
 layer_list_hist <- c("tas_baseline_2000_2020_depthsurf",
@@ -45,9 +45,18 @@ layer_list_hist <- c("tas_baseline_2000_2020_depthsurf",
                      "clt_baseline_2000_2020_depthsurf")
 
 # Define constraints
-constraints_hist <- list(time = c('2000-01-01T00:00:00Z', '2010-01-01T00:00:00Z'),
-                    latitude = c(32, 43),
-                    longitude = c(-126, -116))
+# 10 year time period 
+time_periods_hist <- c(2000) ## 2000: decade 2000-2010
+
+# Latitude and Longitude for california dimensions 
+latitude = c(32, 43)
+longitude = c(-126, -116)
+
+# Define time range 
+time_hist = c(paste0(time_periods_hist, "-01-01T00:00:00Z"),
+         paste0(time_periods_hist, "-01-01T00:00:00Z"))
+
+constraints_hist = list(time_hist, latitude, longitude)
 names(constraints_hist) = c("time", "latitude", "longitude")
 
 # Extract files 
@@ -56,27 +65,40 @@ env_00_list <- map(layer_list_hist, ~{
   subset(layers, grep('_mean', names(layers), value = TRUE))  # Keep only mean layers
 })
 
-# Test that dimensions of rasters layers match before continuing 
-test_that("All raster layers have matching dimensions", {
-  dims <- map(env_00_list, dim) # get dimensions of each raster 
-  first_dim <- dims[[1]] # get the dimensions of just the first layer
+# Test that the rasters are compatible for stacking 
+test_that("All rasters have matching resolution, extent, and CRS", {
+  ref_raster <- env_00_list[[1]]
   
-  all_match <- all(map_lgl(dims, ~ identical(.x, first_dim))) # test that each layer has same dimensions 
+  # Check resolution
+  res_match <- map_lgl(env_00_list, ~ identical(res(.x), res(ref_raster)))
+  expect_true(all(res_match), info = "Not all rasters have the same resolution")
   
-  expect_true(all_match,
-              info = paste0("Not all raster dimensions match, no not move forward with combining into raster stack")
-  )
+  # Check extent
+  ext_match <- map_lgl(env_00_list, ~ identical(ext(.x), ext(ref_raster)))
+  expect_true(all(ext_match), info = "Not all rasters have the same extent")
+  
+  # Check CRS
+  crs_match <- map_lgl(env_00_list, ~ identical(crs(.x), crs(ref_raster)))
+  expect_true(all(crs_match), info = "Not all rasters have the same CRS")
 })
 
+# The extents do not match, but they will once they have been masked to the coastal buffer
 
-# figure out how to subset to coastal buffer shapefile 
-# i predict i can do this by doing a for loop 
-# once it has been masked, then I can write the rasters out to raw data folder
+# Mask historical rasters to coastline buffer ----------------------------------
 
+# Check that CRS of env_00 rasters match ca coastline buffer shapefile 
+if(st_crs(ca_coast) == st_crs(env_00_list[[1]])) {
+  print("The coordinate reference systems match")
+} else {
+  warning("The coordinate reference systems were not a match. Transformation has now occured")
+  ca_coast <- st_transform(ca_coast, st_crs(env_00_list[[1]]))
+}
 
+# Crop and mask hist rasters to ca_coast shapefile 
+env_00_list <- lapply(env_00_list, function(r) {
+  terra::mask(terra::crop(r, ca_coast), ca_coast)
+})
 
-# Combine into one multi-layer SpatRaster 
-#env_00 <- do.call(c, env_00_list)
 
 # Projected data -------------------------------------------------------------
 
@@ -88,14 +110,81 @@ layer_list_proj <- c("tas_ssp460_2020_2100_depthsurf",
                    "so_ssp460_2020_2100_depthsurf",
                    "clt_ssp460_2020_2100_depthsurf")
 
-# Define constraints
-constraints_proj <- list(time = c('2040-01-01T00:00:00Z', '2050-01-01T00:00:00Z'),
-                    latitude = c(32, 43),
-                    longitude = c(-126, -116))
+# 10 year time period 
+time_periods_proj <- c(2050) ## 2050: decade 2050-2060
 
+# Define time range 
+time_proj = c(paste0(time_periods_proj, "-01-01T00:00:00Z"),
+              paste0(time_periods_proj, "-01-01T00:00:00Z"))
+
+constraints_proj = list(time_proj, latitude, longitude)
+names(constraints_proj) = c("time", "latitude", "longitude")
 # Extract files 
 env_50_list <- map(layer_list_proj, ~{
   layers <- download_layers(dataset_id = .x, constraints = constraints_proj, fmt = "raster")
   subset(layers, grep('_mean', names(layers), value = TRUE))  # Keep only mean layers
 })
 
+
+# Test that the rasters are compatible for stacking 
+test_that("All rasters have matching resolution, extent, and CRS", {
+  ref_raster <- env_50_list[[1]]
+  
+  # Check resolution
+  res_match <- map_lgl(env_50_list, ~ identical(res(.x), res(ref_raster)))
+  expect_true(all(res_match), info = "Not all rasters have the same resolution")
+  
+  # Check extent
+  ext_match <- map_lgl(env_50_list, ~ identical(ext(.x), ext(ref_raster)))
+  expect_true(all(ext_match), info = "Not all rasters have the same extent")
+  
+  # Check CRS
+  crs_match <- map_lgl(env_50_list, ~ identical(crs(.x), crs(ref_raster)))
+  expect_true(all(crs_match), info = "Not all rasters have the same CRS")
+})
+
+# The extents do not match, but they will once they have been masked to the coastal buffer
+
+# Mask projected rasters to coastline buffer ----------------------------------
+
+# Check that CRS of env_00 rasters match ca coastline buffer shapefile 
+if(st_crs(ca_coast) == st_crs(env_50_list[[1]])) {
+  print("The coordinate reference systems match")
+} else {
+  warning("The coordinate reference systems were not a match. Transformation has now occured")
+  ca_coast <- st_transform(ca_coast, st_crs(env_50_list[[1]]))
+}
+
+# Crop and mask hist rasters to ca_coast shapefile 
+env_50_list <- lapply(env_50_list, function(r) {
+  terra::mask(terra::crop(r, ca_coast), ca_coast)
+})
+
+
+# Download raster files and upload to Cyberduck -------------------------------
+
+# Define path to historical bio oracle folder on workbench 2 
+outpath_hist <- file.path("/capstone/coastalconservation/data/raw/BioOrc_rasters/historical")
+
+# Write out each raster
+for (i in seq_along(env_00_list)) {
+  terra::writeRaster(
+    env_00_list[[i]],
+    filename = file.path(outpath_hist, paste0(names(env_00_list[[i]]), "_00.tif")),
+    overwrite = TRUE,
+    filetype = "GTiff"
+  )
+}
+
+# Define path to projected bio oracle folder on workbench 2 
+outpath_proj <- file.path("/capstone/coastalconservation/data/raw/BioOrc_rasters/projected_ssp460")
+
+# Write out each raster
+for (i in seq_along(env_50_list)) {
+  terra::writeRaster(
+    env_50_list[[i]],
+    filename = file.path(outpath_proj, paste0(names(env_50_list[[i]]), "_50.tif")),
+    overwrite = TRUE,
+    filetype = "GTiff"
+  )
+}
