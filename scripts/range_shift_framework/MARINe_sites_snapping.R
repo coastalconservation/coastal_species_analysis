36c319f60a868c89bed0c86744afb5cda51f7c47
jcsibley <jordan.c.sibley@gmail.comm>
2025-04-30 22:17:49 +0000

    Push test

--- scripts/range_shift_framework/MARINe_sites_snapping.R
+++ scripts/range_shift_framework/MARINe_sites_snapping.R
@@ -26,6 +26,7 @@ sites <- read_csv('/capstone/coastalconservation/data/raw/MARINe_data/sites/mari
 # Read in CA coastline 
 coastline <- read_sf('/capstone/coastalconservation/data/raw/spatial_data/CA_coastline_polyline/coast.line.CA.shp')
 
+# ADD COMMENT FOR PUSH 
 
 # Prep data -------------------------------------------------------------------
 # CA COASTLINE 
